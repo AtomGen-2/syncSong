4629b971de0c0db2619a8ce52cbdb132b29b57aa
AtomGen-2 <gaonkarsvedant.471@gmail.com>
2021-06-08 08:41:35 +0530

    React Router implementation
    to route using react, every url should redirect you to the index.html page, from that index.html, you can Switch routes once react takes over by throttling control to index.js

--- music_controller/frontend/urls.py
+++ music_controller/frontend/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import index
 
+# Every path redirects to index.html, react takes over. React renders the page by shifting to index.js  
 urlpatterns = [
     path('', index),
+    path('join/', index),
+    path('create/', index),
 ]
